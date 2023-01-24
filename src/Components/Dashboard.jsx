886bde0b2c32679df346e3cb29ef1c56b56a68fb
ajalaritgithub <ajalaajith007@gmail.com>
2023-01-24 15:06:04 +0530

    Update

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
 import Menu from "./Menu";
+import { useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 const Dashboard = () => {
+
+	var [empData, setData] = useState([]);
+        useEffect(() => {
+          fetchEmployeedata();
+        }, []);
+    
+
+    var apLink = "https://jsonplaceholder.typicode.com/users";
+
+          const fetchEmployeedata = () => {
+           axios
+          .get(apLink)
+          .then((response) => {
+            setData(response.data);
+          })
+          .catch();
+        };
 
 	return (
 		<div style={{marginLeft: "8rem",marginRight: "8rem"}}>
@@ -15,54 +35,19 @@ const Dashboard = () => {
 			<tr>
 			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Id</th>
 			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Name</th>
-			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Designation</th>
-			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Location</th>
-			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Salary</th>
+			  <th style={{backgroundColor:"#ff0000", color:"#ffffff", fontSize:30}}>Email</th>
 			</tr>
 		  </thead>
 		  <tbody>
-			<tr>
-			  <td>1</td>
-			  <td>Nikhil</td>
-			  <td>Senior Android Developer</td>
-			  <td>Delhi</td>
-			  <td>15 LPA</td>
-			</tr>
-			<tr>
-			  <td>2</td>
-			  <td>Ajala</td>
-			  <td>Full Stack Developer</td>
-			  <td>Kerala</td>
-			  <td>08 LPA</td>
-			</tr>
-			<tr>
-			  <td>3</td>
-			  <td>Soni</td>
-			  <td>Senior Software Developer</td>
-			  <td>Noida</td>
-			  <td>12 LPA</td>
-			</tr>
-			<tr>
-			  <td>4</td>
-			  <td>Priyanka</td>
-			  <td>Senior Database Administration</td>
-			  <td>Delhi</td>
-			  <td>13 LPA</td>
-			</tr>
-			<tr>
-			  <td>5</td>
-			  <td>Prerna</td>
-			  <td>Senior Operations Manager</td>
-			  <td>Noida</td>
-			  <td>15 LPA</td>
-			</tr>
-			<tr>
-			  <td>6</td>
-			  <td>Bipul</td>
-			  <td>Software Developer</td>
-			  <td>Delhi</td>
-			  <td>09 LPA</td>
-			</tr>
+			{empData.map((item,key) =>{
+                    return(
+                      <tr>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                     </tr>
+                    );
+                  })}
 		  </tbody>
 		</Table>
 		</div>
